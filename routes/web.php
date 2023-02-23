ea64530e0d13298a44b4a1df3976db4b748b0f03
biabrasil <bbia20@outlook.pt>
2023-02-23 11:34:30 +0000

    Correct Main Page

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/posts/{id}', function($id) {
+    return 'Publicação ' . $id;
+})->name('posts.show');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
